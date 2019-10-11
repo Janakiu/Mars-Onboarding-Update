ed4ba02285215f8fcbb61394fcd233c0201f6338
Janaki <janakiu3@gmail.com>
2019-10-11 17:42:51 +1100

    Update and Delete function

---
+++ SpecflowPages/Utils/ConstantUtils.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace SpecflowPages
+{
+    public class ConstantUtils
+    {
+        //Base Url
+        public static string Url = "http://www.skillswap.pro/";
+
+        //ScreenshotPath
+        public static string ScreenshotPath = @"C:\Users\User\Desktop\Janaki-Mars\Mars-OnboardingTask-master\SpecflowPages\TestReports";
+       
+
+        //ExtentReportsPath
+        public static string ReportsPath = @"C:\Users\User\Desktop\Janaki-Mars\Mars-OnboardingTask-master\SpecflowPages\TestReports\Test.html";
+        
+
+
+        //ReportXML Path
+        public static string ReportXMLPath = @"C:\Users\User\Desktop\Janaki-Mars\Mars-OnboardingTask-master\SpecflowPages\TestReports\ReportXML.xml";
+        
+
+
+    }
+}
---
+++ SpecflowTests/AcceptanceTest/Feature/AddLang.feature
@@ -0,0 +1,23 @@
+﻿Feature: Add Language
+	In order to update my profile 
+	As a skill trader
+	I want to add,edit and delete the language
+
+@mytag
+Scenario: Check if user could able to add a language 
+	Given I clicked on the Language tab under Profile page
+	When  I add a new language
+	Then that language should be displayed on my listings
+
+@mytag 
+Scenario: Check if user could able to update language 
+	Given I clicked on the Language tab under Profile page
+	When I want to edit exsisting language
+	Then that language should be displayed on my listings
+	
+@mytag 
+Scenario: Check if user could able to delete language 
+	Given I clicked on the Language tab under Profile page
+	When I want to delete exsisting language
+	Then that language should not be be displayed on my listings
+	
